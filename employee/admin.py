45d49431f1c79bea31a7ae3558f123953d88d19a
Vishesh Agarwal <visheshagarwal600@gmail.com>
2024-08-12 17:26:24 +0530

    Revert "Created the Interview Portal."
    This reverts commit 647691f0bb48fb0afaf5739ae41da8244d9b6962.

--- employee/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Employee
-
-# Register your models here.
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['employee_name', 'employee_email', 'employee_skill', 'employee_level']
